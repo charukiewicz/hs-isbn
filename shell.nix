32981d3e5d866df75464f164b5bd86fb532dc40f
charukiewicz <c.charukiewicz@gmail.com>
2020-05-10 18:15:39 -0500

    refactored shell.nix as function

--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
 let
-  pkgs = import (builtins.fetchGit {
-    url = https://github.com/NixOS/nixpkgs-channels;
-    ref = "nixos-20.03";
-    rev = "4d373182597cff60b3a820affb5a73dd274e205b";
-  }) {};
+  defaultPkgs = import (builtins.fetchGit {
+      url = https://github.com/NixOS/nixpkgs-channels;
+      ref = "nixos-20.03";
+      rev = "4d373182597cff60b3a820affb5a73dd274e205b";
+    }) {};
 in
+{ pkgs ? defaultPkgs }:
 pkgs.haskellPackages.developPackage
   { root = ./.;
     overrides = self: super: { };
